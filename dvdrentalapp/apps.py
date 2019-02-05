b7bb0e470e0c26f9db005bbd7df5ef07aed1a814
ManarOmar <manar.aljawarneh@gmail.com>
2019-02-06 00:05:04 +0200

    creating models file by inspecting the database

---
+++ dvdrentalapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DvdrentalappConfig(AppConfig):
+    name = 'dvdrentalapp'
---
+++ dvdrentalapp/models.py
@@ -0,0 +1,203 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+
+class Actor(models.Model):
+    actor_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    last_update = models.DateTimeField()
+    middle_name = models.CharField(max_length=45, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'actor'
+
+
+class Address(models.Model):
+    address_id = models.AutoField(primary_key=True)
+    address = models.CharField(max_length=50)
+    address2 = models.CharField(max_length=50, blank=True, null=True)
+    district = models.CharField(max_length=20)
+    city = models.ForeignKey('City', models.DO_NOTHING)
+    postal_code = models.CharField(max_length=10, blank=True, null=True)
+    phone = models.CharField(max_length=20)
+    last_update = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'address'
+
+
+class Category(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=25)
+    last_update = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'category'
+
+
+class City(models.Model):
+    city_id = models.AutoField(primary_key=True)
+    city = models.CharField(max_length=50)
+    country = models.ForeignKey('Country', models.DO_NOTHING)
+    last_update = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'city'
+
+
+class Country(models.Model):
+    country_id = models.AutoField(primary_key=True)
+    country = models.CharField(max_length=50)
+    last_update = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'country'
+
+
+class Customer(models.Model):
+    customer_id = models.AutoField(primary_key=True)
+    store_id = models.SmallIntegerField()
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    email = models.CharField(max_length=50, blank=True, null=True)
+    address = models.ForeignKey(Address, models.DO_NOTHING)
+    activebool = models.BooleanField()
+    create_date = models.DateField()
+    last_update = models.DateTimeField(blank=True, null=True)
+    active = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'customer'
+
+
+class Film(models.Model):
+    film_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    release_year = models.IntegerField(blank=True, null=True)
+    language = models.ForeignKey('Language', models.DO_NOTHING)
+    rental_duration = models.SmallIntegerField()
+    rental_rate = models.DecimalField(max_digits=4, decimal_places=2)
+    length = models.SmallIntegerField(blank=True, null=True)
+    replacement_cost = models.DecimalField(max_digits=5, decimal_places=2)
+    rating = models.TextField(blank=True, null=True)  # This field type is a guess.
+    last_update = models.DateTimeField()
+    special_features = models.TextField(blank=True, null=True)  # This field type is a guess.
+    fulltext = models.TextField()  # This field type is a guess.
+
+    class Meta:
+        managed = False
+        db_table = 'film'
+
+
+class FilmActor(models.Model):
+    actor = models.ForeignKey(Actor, models.DO_NOTHING, primary_key=True)
+    film = models.ForeignKey(Film, models.DO_NOTHING)
+    last_update = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'film_actor'
+        unique_together = (('actor', 'film'),)
+
+
+class FilmCategory(models.Model):
+    film = models.ForeignKey(Film, models.DO_NOTHING, primary_key=True)
+    category = models.ForeignKey(Category, models.DO_NOTHING)
+    last_update = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'film_category'
+        unique_together = (('film', 'category'),)
+
+
+class Inventory(models.Model):
+    inventory_id = models.AutoField(primary_key=True)
+    film = models.ForeignKey(Film, models.DO_NOTHING)
+    store_id = models.SmallIntegerField()
+    last_update = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'inventory'
+
+
+class Language(models.Model):
+    language_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+    last_update = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'language'
+
+
+class Payment(models.Model):
+    payment_id = models.AutoField(primary_key=True)
+    customer = models.ForeignKey(Customer, models.DO_NOTHING)
+    staff = models.ForeignKey('Staff', models.DO_NOTHING)
+    rental = models.ForeignKey('Rental', models.DO_NOTHING)
+    amount = models.DecimalField(max_digits=5, decimal_places=2)
+    payment_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'payment'
+
+
+class Rental(models.Model):
+    rental_id = models.AutoField(primary_key=True)
+    rental_date = models.DateTimeField()
+    inventory = models.ForeignKey(Inventory, models.DO_NOTHING)
+    customer = models.ForeignKey(Customer, models.DO_NOTHING)
+    return_date = models.DateTimeField(blank=True, null=True)
+    staff = models.ForeignKey('Staff', models.DO_NOTHING)
+    last_update = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'rental'
+        unique_together = (('rental_date', 'inventory', 'customer'),)
+
+
+class Staff(models.Model):
+    staff_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    address = models.ForeignKey(Address, models.DO_NOTHING)
+    email = models.CharField(max_length=50, blank=True, null=True)
+    store_id = models.SmallIntegerField()
+    active = models.BooleanField()
+    username = models.CharField(max_length=16)
+    password = models.CharField(max_length=40, blank=True, null=True)
+    last_update = models.DateTimeField()
+    picture = models.BinaryField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'staff'
+
+
+class Store(models.Model):
+    store_id = models.AutoField(primary_key=True)
+    manager_staff = models.ForeignKey(Staff, models.DO_NOTHING, unique=True)
+    address = models.ForeignKey(Address, models.DO_NOTHING)
+    last_update = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'store'
